46ca4d1bfb3b0d77e004b93b99e00eccf8b0defa
Dani Poquet <dpoquet8@gmail.com>
2019-10-08 00:13:47 +0200

    Added CSS-loader and optimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
 const webpack = require('webpack');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const config = {
   entry: {
       main: './src/main.js',
       home: './src/home.js',
-      login: './src/login.js'
+      login: './src/login.js',
+      styles: './src/styles/styles.css'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
+  plugins: [
+    new MiniCssExtractPlugin({filename: "styles/[name].css"}),
+    new FixStyleOnlyEntriesPlugin(),
+    new OptimizeCSSAssetsPlugin({})
+  ],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /(node_modules|bower_components)/,
-      }
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /(node_modules|bower_components)/,
+        },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader,"css-loader"]
+        }
     ]
   }
 }
